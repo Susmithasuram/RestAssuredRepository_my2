9a7343de912e955183d95b63cc7823a7ee6101c3
Susmtha Suram <susmitha.suram7@gmail.com>
2024-09-18 12:42:02 +0530

    one more file added

---
+++ src/test/java/tests/Sampletest2.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Sampletest2
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("****************running sample test class**************8");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("******running test 2 method from sampletest class****************");
+	}
+
+}
