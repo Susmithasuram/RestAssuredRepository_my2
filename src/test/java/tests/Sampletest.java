9c2f89f91bded94b2a9c2f37c18e7a55afd8a9ea
Susmtha Suram <susmitha.suram7@gmail.com>
2024-09-18 12:37:09 +0530

    update sampletest file

--- src/test/java/tests/Sampletest.java
+++ src/test/java/tests/Sampletest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class Sampletest {
+import org.testng.annotations.Test;
+
+public class Sampletest
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("****************running sample test class**************8");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("******running test 2 method from sampletest class****************");
+	}
 
 }
