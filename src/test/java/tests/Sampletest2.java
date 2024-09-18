44882f13daf15395aa838934aa51edf7c16e8eb7
Susmtha Suram <susmitha.suram7@gmail.com>
2024-09-18 12:48:58 +0530

    updated sample files

--- src/test/java/tests/Sampletest2.java
+++ src/test/java/tests/Sampletest2.java
@@ -7,12 +7,17 @@ public class Sampletest2
 	@Test
 	public void test1()
 	{
-		System.out.println("****************running sample test class**************8");
+		System.out.println("****************running test1 method sample test2 class**************8");
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("******running test 2 method from sampletest class****************");
+		System.out.println("******running test 2 method from sampletest2 class****************");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("******running test 3 method from sampletest2 class****************");
 	}
 
 }
